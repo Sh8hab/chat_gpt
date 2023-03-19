1ddf92939f7f58c7cd5c11f023aa0a21b7d959a6
Sh8hab <shehabhoba11@gmail.com>
2023-03-19 20:36:02 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:chat_gpt/dio_helper.dart';
+import 'package:chat_gpt/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  DioHelper.intDio();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(
+      ),
+      home: const HomePage(),
+    );
+  }
+}
